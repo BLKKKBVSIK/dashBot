8a9836bd832bc047a686c031d0d9600d0da2cb4e
Enzo CONTY <contact@enzoconty.dev>
2020-08-28 02:07:28 +0200

    :sparkles: Setting presence, ready to create commands.

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,22 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:nyxx/nyxx.dart';
+import 'package:nyxx_commander/commander.dart';
+
+import 'commands/dart/exec_dart.dart';
+import 'commands/flutter/exec_flutter.dart';
+import 'misc/config.dart' as config;
+
+void main() {
+  var bot = Nyxx(config.token!, options: ClientOptions(shardCount: 1));
+
+  bot.onReady.listen((ReadyEvent e) {
+    print('Ready !');
+    bot.setPresence(
+      PresenceBuilder.of(
+        game: Activity.of('${config.prefix}dart | ${config.prefix}flutter',
+            type: ActivityType.listening),
+      ),
+    );
+  });
+
+  Commander(bot, prefix: config.prefix);
 }
---
+++ bin/misc/config.dart
@@ -0,0 +1,5 @@
+import 'dart:io';
+
+String? get prefix => Platform.environment['PREFIX'];
+String? get token => Platform.environment['TOKEN'];
+String? get adminId => Platform.environment['ADMIN_ID'];
