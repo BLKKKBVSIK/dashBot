5abecfb0b0c697a04dc7239a353d42a5bb984690
Enzo CONTY <contact@enzoconty.dev>
2020-08-29 23:14:57 +0200

    🔧 Add env variable 'DASH_GITHUB_TOKEN'

--- bin/misc/config.dart
+++ bin/misc/config.dart
@@ -2,3 +2,4 @@ import 'dart:io';
 
 String? get prefix => Platform.environment['DASH_PREFIX'];
 String? get token => Platform.environment['DASH_TOKEN'];
+String? get ghToken => Platform.environment['DASH_GITHUB_TOKEN'];
