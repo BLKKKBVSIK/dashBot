20bf1e137dbca1ef0f3d1d4f2e3f3137a36239a8
Enzo CONTY <contact@enzoconty.dev>
2020-08-29 23:45:14 +0200

    :bug: Remove discord markdown magic quote

--- bin/misc/filter_content.dart
+++ bin/misc/filter_content.dart
@@ -4,5 +4,6 @@ String filterContent(String content) {
   String filteredContent;
   
   filteredContent = content.substring(config.prefix!.length + 'flutter '.length);
+  filteredContent = filteredContent.replaceAll(RegExp(r'`'), '');
   return filteredContent;
 }
