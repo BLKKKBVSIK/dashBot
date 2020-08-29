4cf2b225245cd46c0644e06c4ebdbf815be28038
Enzo CONTY <contact@enzoconty.dev>
2020-08-29 23:14:34 +0200

    🔧 Remove env variable 'DASH_ADMIN_ID'

--- bin/misc/config.dart
+++ bin/misc/config.dart
@@ -2,4 +2,3 @@ import 'dart:io';
 
 String? get prefix => Platform.environment['DASH_PREFIX'];
 String? get token => Platform.environment['DASH_TOKEN'];
-String? get adminId => Platform.environment['DASH_ADMIN_ID'];
